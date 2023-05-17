65ad4660bed975b248e824ab14115847e117ca2e
devikaDTS <128971635+devikaDTS@users.noreply.github.com>
2023-05-17 18:42:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    mustacha = loadImage("https://i.postimg.cc/d3RG9YYN/m.png");
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet( video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
+function draw()
+{
+    image(video, 0,0,300,300);
+    image(mustacha, noseX-40, noseY-15, 80, 50);
+}
+
+function take_snapshot()
+{
+    save('MyFilterImage.png');
+}
